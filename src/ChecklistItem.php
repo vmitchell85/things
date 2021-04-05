6a5b66909c4a6f3e5e24f464266f7a4572bd59f2
Vince Mitchell <vincent.mitchell@gmail.com>
2021-04-04 21:39:33 -0400

    add scopes

--- src/ChecklistItem.php
+++ src/ChecklistItem.php
@@ -10,11 +10,21 @@ class ChecklistItem extends Model
     protected $table = 'TMChecklistItem';
     protected $primaryKey = 'uuid';
     protected $keyType = 'string';
-
+ 
     public $incrementing = false;
 
     public function rTask()
     {
         return $this->belongsTo(Task::class, 'task');
+    }
+
+    public function scopeIncomplete($query)
+    {
+        return $query->where('status', 0);
+    }
+
+    public function scopeComplete($query)
+    {
+        return $query->where('status', 3);
     }
 }
